278a72f918ea83b1f52a3c3920bb000567389d40
suryo-harianja <suryo@aleph-labs.com>
2021-07-15 19:21:46 +0700

    upload first commit

---
+++ web/sites/default/files/js/js_4E-JJ-n9Jik8zBsMawCIpVXvzSLb-udYxhd7-8Iq-rg.js
@@ -0,0 +1,105 @@
+/**
+ * @file
+ * JavaScript behaviors for element help text (tooltip).
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  // @see https://atomiks.github.io/tippyjs/v5/all-props/
+  // @see https://atomiks.github.io/tippyjs/v6/all-props/
+  Drupal.webform = Drupal.webform || {};
+  Drupal.webform.elementHelpIcon = Drupal.webform.elementHelpIcon || {};
+  Drupal.webform.elementHelpIcon.options = Drupal.webform.elementHelpIcon.options || {};
+
+  /**
+   * Element help icon.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webformElementHelpIcon = {
+    attach: function (context) {
+      if (!window.tippy) {
+        return;
+      }
+
+      $(context).find('.js-webform-element-help').once('webform-element-help').each(function () {
+        var $link = $(this);
+
+        $link.on('click', function (event) {
+          // Prevent click from toggling <label>s wrapped around help.
+          event.preventDefault();
+        });
+
+        var options = $.extend({
+          content: $link.attr('data-webform-help'),
+          delay: 100,
+          allowHTML: true,
+          interactive: true
+        }, Drupal.webform.elementHelpIcon.options);
+
+        tippy(this, options);
+      });
+    }
+  };
+
+})(jQuery, Drupal);
+;
+/**
+ * @file
+ * JavaScript behaviors for options elements.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Attach handlers to options buttons element.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webformOptionsButtons = {
+    attach: function (context) {
+      // Place <input> inside of <label> before the label.
+      $(context).find('label.webform-options-display-buttons-label > input[type="checkbox"], label.webform-options-display-buttons-label > input[type="radio"]').each(function () {
+        var $input = $(this);
+        var $label = $input.parent();
+        $input.detach().insertBefore($label);
+      });
+    }
+  };
+
+
+})(jQuery, Drupal);
+;
+/**
+ * @file
+ * JavaScript behaviors for select menu.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Disable select menu options using JavaScript.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webformSelectOptionsDisabled = {
+    attach: function (context) {
+      $('select[data-webform-select-options-disabled]', context).once('webform-select-options-disabled').each(function () {
+        var $select = $(this);
+        var disabled = $select.attr('data-webform-select-options-disabled').split(/\s*,\s*/);
+        $select.find('option').filter(function isDisabled() {
+          return ($.inArray(this.value, disabled) !== -1);
+        }).attr('disabled', 'disabled');
+      });
+    }
+  };
+
+
+})(jQuery, Drupal);
+;
